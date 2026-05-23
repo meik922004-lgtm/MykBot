3cb734fa0eb9127ec13e9f0f6f79248a7c56745b
Meik <meik922004@gmail.com>
2026-05-23 21:24:31 +0700

    Fix: Add tlsAllowInvalidCertificates to bypass MongoDB SSL error on Render

--- Database.py
+++ Database.py
@@ -9,5 +9,9 @@ if not MONGO_URI:
     # Nếu không tìm thấy biến môi trường, hệ thống sẽ lấy chuỗi mặc định của bạn để chạy ổn định
     MONGO_URI = "mongodb+srv://meik922004_db_user:LrXxnoloY8TaezNI@database0.gjbsfwh.mongodb.net/?appName=database0"
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+# SỬA TẠI ĐÂY: Thêm tham số tlsAllowInvalidCertificates=True vào Client để vượt qua lỗi SSL trên Render
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    MONGO_URI, 
+    tlsAllowInvalidCertificates=True
+)
 db = client.database0
