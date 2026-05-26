bee0421e9cd821a34c9e0bf43b643e655bdf9b90
Meik <meik922004@gmail.com>
2026-05-26 21:20:53 +0700

    try fixing with db

--- Database.py
+++ Database.py
@@ -3,15 +3,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-MONGO_URI = os.getenv("MONGO_URI")
 
+MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
-    # Nếu không tìm thấy biến môi trường, hệ thống sẽ lấy chuỗi mặc định của bạn để chạy ổn định
     MONGO_URI = "mongodb+srv://meik922004_db_user:LrXxnoloY8TaezNI@database0.gjbsfwh.mongodb.net/?appName=database0"
 
-# SỬA TẠI ĐÂY: Thêm tham số tlsAllowInvalidCertificates=True vào Client để vượt qua lỗi SSL trên Render
 client = motor.motor_asyncio.AsyncIOMotorClient(
     MONGO_URI, 
     tlsAllowInvalidCertificates=True
 )
+
 db = client.database0
+
+# Thêm 3 dòng này vào cuối file
+players_col = db.players
+parties_col = db.parties
+dungeon_configs = db.dungeon_configs
